3978ee4efe4634547d348676f53d305b12a58589
Sefirosu <hayden.oneill9@gmail.com>
2014-09-05 17:40:51 +1200

    added average case tests

--- tester.rb
+++ tester.rb
@@ -14,6 +14,10 @@ class Tester
     @tests = @tests << {:type => 1, :x => x, :y => y, :e_c => exp_cost, :n => name}
   end
 
+  def add_perform_test(x, y, name)
+    @tests = @tests << {:type => 2,:x => x, :y => y, :n => name}
+  end
+
   def run_tests
     successes = 0
     failures = 0
@@ -21,6 +25,7 @@ class Tester
       res = false
       res = test_genes test[:x], test[:y], test[:e_x], test[:e_y], test[:e_c], test[:n], i+1 if test[:type] == 0
       res = test_general_genes test[:x], test[:y], test[:e_c], test[:n], i+1 if test[:type] == 1
+      res = test_performance_genes test[:x], test[:y], test[:n], i+1 if test[:type] == 2
       successes = successes + 1 if res
       failures = failures + 1 if not res
     end
@@ -33,14 +38,14 @@ class Tester
     start = Time.now
     result = GeneAligner.align_genes x, y
     span = Time.now - start
-#   span_str = Time.at(span).strftime("%9N")
+    #   span_str = Time.at(span).strftime("%9N")
     success = (result[x] == exp_x) && (result[y] == exp_y) && (result[:cost_string] == exp_cost)
     status = success ? "SUCCESS" : "FAILURE"
     a_step = GeneAligner.align_step
     r_step = GeneAligner.retrieve_step
     GeneAligner.reset_barometers
     print "#{status} TEST#{'%03d' % num}: #{'%30s' % name}, took: #{'%.4f' % (span)}s, n = #{x.length > y.length ? x.length : y.length} a-step: #{a_step} r-step: #{r_step}\n"
-#   print "#{status} TEST#{'%03d' % num}: #{'%30s' % name}, took: #{'%.3f' % (span_str.to_f/1000000)}ms, n = #{x.length > y.length ? x.length : y.length} a-step: #{a_step} r-step: #{r_step}\n"
+    #   print "#{status} TEST#{'%03d' % num}: #{'%30s' % name}, took: #{'%.3f' % (span_str.to_f/1000000)}ms, n = #{x.length > y.length ? x.length : y.length} a-step: #{a_step} r-step: #{r_step}\n"
     print "expected:#{exp_cost}, got:#{result[:cost_string]}\n#{result[:string]}\n" if !success
     success
   end
@@ -49,7 +54,7 @@ class Tester
     start = Time.now
     result = GeneAligner.align_genes x, y
     span = Time.now - start
-#   span_str = Time.at(span).strftime("%9N")
+    #   span_str = Time.at(span).strftime("%9N")
     success = (result[:cost] == exp_cost)
     status = success ? "SUCCESS" : "FAILURE"
     a_step = GeneAligner.align_step
@@ -58,5 +63,16 @@ class Tester
     print "#{status} TEST#{'%03d' % num}: #{'%30s' % name}, took: #{'%.4f' % (span)}s, n = #{x.length > y.length ? x.length : y.length} a-step: #{a_step} r-step: #{r_step}\n"
     print "expected:#{exp_cost}, got:#{result[:cost]}\n#{result[:string]}\n" if !success
     success
+  end
+
+  def test_performance_genes(x,y,name,num)
+    start = Time.now
+    result = GeneAligner.align_genes x, y
+    span = Time.now - start
+    a_step = GeneAligner.align_step
+    r_step = GeneAligner.retrieve_step
+    GeneAligner.reset_barometers
+    print "PERFORM TEST#{'%03d' % num}: #{'%30s' % name}, took: #{'%.4f' % (span)}s, n = #{x.length > y.length ? x.length : y.length} a-step: #{a_step} r-step: #{r_step}\n"
+    true
   end
 end
